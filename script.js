28f5b33c218af42565615632fcde55c196e5483b
sixfacearu <sixface0324@gmail.com>
2020-03-25 00:11:49 +0530

    change delete function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var cardId=0;
 (function($) {
     $(document).ready(function() {
         var $chatbox = $('.chatbox'),
@@ -10,16 +11,15 @@
         });
         $chatboxTitleClose.on('click', function(e) {
             e.stopPropagation();
-            $chatbox.addClass('chatbox--closed');
+            if($('.chatbox').hasClass("chatbox--tray")==false){
+                $chatbox.toggleClass('chatbox--tray');
+            }
         });
         $chatbox.on('transitionend', function() {
             if ($chatbox.hasClass('chatbox--closed')) $chatbox.remove();
         });
         
     });
-
-    
-    
 })(jQuery);
 
 function enterButtonEvent() {
@@ -31,13 +31,27 @@ function enterButtonEvent() {
 }
 function sendMsg() {
    var notes=jQuery("#btn-input").val();
-   
+   var shortNotes=notes;
+   var content=null;
    if(notes.length>0){
-   var content='<div class="chatbox__body__message chatbox__body__message--left"><div class="chatbox_timing"><ul><li><a href="#"><i class="fa fa-calendar"></i>'+getDate()+'</a></li><li><a href="#"><i class="fa fa-clock-o"></i>' +getTime()+'</a></li></ul></div><div class="clearfix"></div><div class="ul_section_full"><ul class="ul_msg"><li><strong>Your Note</strong></li><li>'+notes+'</li></ul><div class="clearfix"></div><ul class="ul_msg2"><li><a href="#"><i class="fa fa-pencil"></i> </a></li><li><a href="#"><i class="fa fa-trash chat-trash"></i></a></li></ul></div></div>';
+   shortNotes =(notes.length>20)? notes.substring(0, 20) + " ...":notes;
+   content='<div class="chatbox__body__message chatbox__body__message--left" id="card_body'+cardId+'"><div class="chatbox_timing"><ul><li><a href="#"><i class="fa fa-calendar"></i>'+getDate()+'</a></li><li><a href="#"><i class="fa fa-clock-o"></i>' +getTime()+'</a></li></ul></div><div class="clearfix"></div><div class="ul_section_full"><ul class="ul_msg"><li><strong>Your Note</strong></li><li>'+shortNotes+'</li></ul><div class="clearfix"></div><ul class="ul_msg2"><li><a href="#"><i class="fa fa-pencil"></i> </a></li><li><a href="#" id='+cardId+'><i class="fa fa-trash chat-trash"></i></a></li></ul></div></div>';
     $(".chatbox__body").append(content);
     jQuery("#btn-input").val('');
+    addEventListener(cardId);
+    cardId=cardId+1;
    }
 }
+
+function addEventListener(id) {
+    if(document.getElementById(id)){
+        var parent="card_body"+id;
+        jQuery("#"+parent+" > div.ul_section_full > ul.ul_msg2 > li:nth-child(2) > #"+id).click(function(){
+            jQuery("#"+parent+" > div.ul_section_full > ul.ul_msg2 > li:nth-child(2) > #"+id)[0].offsetParent.remove();
+        });
+    }
+}
+
 
 function getDate() {
     var currentdate = new Date(); 
